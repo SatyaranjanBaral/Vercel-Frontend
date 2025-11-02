07428e0ea6f21a681b9152ab2ce742ddfb14e1ee
Satyaranjan Baraj <satyaranjanbaral76@gmail.com>
2025-11-02 22:32:16 +0530

    added

--- src/config.js
+++ src/config.js
@@ -1,2 +1,2 @@
-export const BASE_URL = "http://localhost:5000/api/v1";
+export const BASE_URL = "https://vercel.com/satyaranjanbarals-projects/vercel-backend/BYJ3nYycaa5DuBXzvdUmFsomh8kG/api/v1";
 export const token = localStorage.getItem("token");
